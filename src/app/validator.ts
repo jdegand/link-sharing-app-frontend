8ee944f43aa606ebfcb4abdad5a97a2bdca083ba
jdegand <70610011+jdegand@users.noreply.github.com>
2024-01-17 14:26:54 -0500

    delete: validator file

--- src/app/validator.ts
+++
@@ -1,8 +0,0 @@
-import { ValidatorFn, AbstractControl, ValidationErrors, FormGroup } from "@angular/forms";
-
-export const platformIncludedValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-    const formGroup = control as FormGroup;
-    const platform = formGroup.get('platform');
-    const url = formGroup.get('url');
-    return url?.value === "red" ? { platformIncludedValidator: true } : null;
-};
