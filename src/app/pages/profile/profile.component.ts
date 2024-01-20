67fda063c5820c7b29be4d3c5e44ed90dfccde30
jdegand <70610011+jdegand@users.noreply.github.com>
2024-01-19 20:53:08 -0500

    refactor: add profile form

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,19 +3,31 @@ import { RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api/api.service';
 import { AuthService } from '../../services/auth/auth.service';
 import { ButtonModule } from 'primeng/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink, ButtonModule],
+  imports: [RouterLink, ButtonModule, ReactiveFormsModule, InputTextModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
   authService = inject(AuthService);
   apiService = inject(ApiService);
+  fb = inject(FormBuilder);
+
+  profileForm!: FormGroup;
 
   ngOnInit() {
+    this.profileForm = this.fb.group({
+      firstname: [this.authService.currentUserSig()?.firstname, Validators.required],
+      lastname: [this.authService.currentUserSig()?.lastname, Validators.required],
+      email: [this.authService.currentUserSig()?.email],
+      image: [this.authService.currentUserSig()?.image],
+    });
+
     this.apiService.getUser()
       .subscribe({
         next: (response) => {
@@ -26,6 +38,13 @@ export class ProfileComponent implements OnInit {
           this.authService.currentUserSig.set(null);
         },
       });
+  }
+
+  onSubmit() {
+    // valid is not enough when you pre-fill all the inputs
+    if (this.profileForm.valid && this.profileForm.touched) {
+      console.log(this.profileForm)
+    }
   }
 
 }
